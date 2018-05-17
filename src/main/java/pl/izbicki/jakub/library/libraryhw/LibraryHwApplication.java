3abe63a733947f7d29e310623de3a6f7ae855790
Jakub-Izbicki <jakub.izbicki1@gmail.com>
2018-05-17 17:35:02 +0200

    Init commit.

---
+++ src/main/java/pl/izbicki/jakub/library/libraryhw/LibraryHwApplication.java
@@ -0,0 +1,12 @@
+package pl.izbicki.jakub.library.libraryhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryHwApplication.class, args);
+	}
+}
